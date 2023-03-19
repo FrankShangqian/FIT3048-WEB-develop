43a1e18d7de0abee40a56cd0615b02880c60f73c
sduu0007 <sduu0007@student.monash.edu>
2023-03-19 22:51:54 +1100

    template

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -1,57 +1,254 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- * @var \App\View\AppView $this
- */
-
-$cakeDescription = 'CakePHP: the rapid development php framework';
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <?= $this->Html->charset() ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?= $cakeDescription ?>:
-        <?= $this->fetch('title') ?>
-    </title>
+    <meta charset="utf-8" />
+    <title><?= $this->fetch('title') ?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
     <?= $this->Html->meta('icon') ?>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
+    <link href="css/styles.css" rel="stylesheet" />
 
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
-
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+    <?= $this->Html->css('css/styles.css') ?>
+    <link href="https://fonts.googleapis. com/css?family=Raleway:400,700 rel="stylesheet">
+    <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <?= $this->Html->css(['styles', 'cake','bootstrap.min.css']) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
+
+    <?= $this->Html->script(['https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', 'scripts',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js'
+        ,'https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js','bootstrap.min.js']) ?>
 </head>
-<body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
+<body class="nav-fixed">
+<nav class="topnav navbar navbar-expand shadow justify-content-between justify-content-sm-start navbar-light bg-white" id="sidenavAccordion">
+    <!-- Sidenav Toggle Button-->
+    <!-- Navbar Brand-->
+    <!-- * * Tip * * You can use text or an image for your navbar brand.-->
+    <!-- * * * * * * When using an image, we recommend the SVG format.-->
+    <!-- * * * * * * Dimensions: Maximum height: 32px, maximum width: 240px-->
+    <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="$this->Url->build('/home')">MONO Admin & contractor</a>
+    <!-- Navbar Search Input-->
+    <!-- * * Note: * * Visible only on and above the lg breakpoint-->
+    <form class="form-inline me-auto d-none d-lg-block me-3">
+
+    </form>
+    <!-- Navbar Items-->
+    <ul class="navbar-nav align-items-center ms-auto">
+        <!-- Documentation Dropdown-->
+
+        <!-- Navbar Search Dropdown-->
+        <!-- * * Note: * * Visible only below the lg breakpoint-->
+        <li class="nav-item dropdown no-caret me-3 d-lg-none">
+            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="searchDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="search"></i></a>
+            <!-- Dropdown - Search-->
+            <div class="dropdown-menu dropdown-menu-end p-3 shadow animated--fade-in-up" aria-labelledby="searchDropdown">
+                <form class="form-inline me-auto w-100">
+                    <div class="input-group input-group-joined input-group-solid">
+                        <input class="form-control pe-0" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
+                        <div class="input-group-text"><i data-feather="search"></i></div>
+                    </div>
+                </form>
+            </div>
+        </li>
+        <!-- Alerts Dropdown-->
+        <li class="nav-item dropdown no-caret d-none d-sm-block me-3 dropdown-notifications">
+            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownAlerts" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="bell"></i></a>
+            <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownAlerts">
+                <h6 class="dropdown-header dropdown-notifications-header">
+                    <i class="me-2" data-feather="bell"></i>
+                    Alerts Center
+                </h6>
+
+                <a class="dropdown-item dropdown-notifications-footer" href="#!">View All Alerts</a>
+            </div>
+        </li>
+        <!-- Messages Dropdown-->
+        <li class="nav-item dropdown no-caret d-none d-sm-block me-3 dropdown-notifications">
+            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownMessages" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="mail"></i></a>
+            <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownMessages">
+                <h6 class="dropdown-header dropdown-notifications-header">
+                    <i class="me-2" data-feather="mail"></i>
+                    Message Center
+                </h6>
+                <!-- Example Message 1  -->
+                <a class="dropdown-item dropdown-notifications-item" href="#!">
+                    <img class="dropdown-notifications-item-img" src="assets/img/illustrations/profiles/profile-2.png" />
+                    <div class="dropdown-notifications-item-content">
+                        <div class="dropdown-notifications-item-content-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div class="dropdown-notifications-item-content-details">Thomas Wilcox · 58m</div>
+                    </div>
+                </a>
+                <!-- Example Message 2-->
+                <a class="dropdown-item dropdown-notifications-item" href="#!">
+                    <img class="dropdown-notifications-item-img" src="assets/img/illustrations/profiles/profile-3.png" />
+                    <div class="dropdown-notifications-item-content">
+                        <div class="dropdown-notifications-item-content-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div class="dropdown-notifications-item-content-details">Emily Fowler · 2d</div>
+                    </div>
+                </a>
+                <!-- Example Message 3-->
+                <a class="dropdown-item dropdown-notifications-item" href="#!">
+                    <img class="dropdown-notifications-item-img" src="assets/img/illustrations/profiles/profile-4.png" />
+                    <div class="dropdown-notifications-item-content">
+                        <div class="dropdown-notifications-item-content-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div class="dropdown-notifications-item-content-details">Marshall Rosencrantz · 3d</div>
+                    </div>
+                </a>
+                <!-- Example Message 4-->
+                <a class="dropdown-item dropdown-notifications-item" href="#!">
+                    <img class="dropdown-notifications-item-img" src="assets/img/illustrations/profiles/profile-5.png" />
+                    <div class="dropdown-notifications-item-content">
+                        <div class="dropdown-notifications-item-content-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div class="dropdown-notifications-item-content-details">Colby Newton · 3d</div>
+                    </div>
+                </a>
+                <!-- Footer Link-->
+                <a class="dropdown-item dropdown-notifications-footer" href="#!">Read All Messages</a>
+            </div>
+        </li>
+        <!-- User Dropdown-->
+        <li class="nav-item dropdown no-caret dropdown-user me-3 me-lg-4">
+            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="assets/img/illustrations/profiles/profile-1.png" /></a>
+            <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownUserImage">
+                <h6 class="dropdown-header d-flex align-items-center">
+                    <img class="dropdown-user-img" src="assets/img/illustrations/profiles/profile-1.png" />
+                    <div class="dropdown-user-details">
+                        <div class="dropdown-user-details-name">Valerie Luna</div>
+                        <div class="dropdown-user-details-email">vluna@aol.com</div>
+                    </div>
+                </h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#!">
+                    <div class="dropdown-item-icon"><i data-feather="settings"></i></div>
+                    Account
+                </a>
+                <a class="dropdown-item" href="#!">
+                    <div class="dropdown-item-icon"><i data-feather="log-out"></i></div>
+                    Logout
+                </a>
+            </div>
+        </li>
+    </ul>
+</nav>
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sidenav shadow-right sidenav-light">
+            <div class="sidenav-menu">
+                <div class="nav accordion" id="accordionSidenav">
+                    <!-- Sidenav Menu Heading (Account)-->
+                    <!-- * * Note: * * Visible only on and above the sm breakpoint-->
+                    <div class="sidenav-menu-heading d-sm-none">Account</div>
+                    <!-- Sidenav Link (Alerts)-->
+                    <!-- * * Note: * * Visible only on and above the sm breakpoint-->
+                    <a class="nav-link d-sm-none" href="#!">
+                        <div class="nav-link-icon"><i data-feather="bell"></i></div>
+                        Alerts
+                        <span class="badge bg-warning-soft text-warning ms-auto">4 New!</span>
+                    </a>
+                    <!-- Sidenav Link (Messages)-->
+                    <!-- * * Note: * * Visible only on and above the sm breakpoint-->
+                    <a class="nav-link d-sm-none" href="#!">
+                        <div class="nav-link-icon"><i data-feather="mail"></i></div>
+                        Messages
+                        <span class="badge bg-success-soft text-success ms-auto">2 New!</span>
+                    </a>
+                    <!-- Sidenav Menu Heading (Core)-->
+                    <div class="sidenav-menu-heading">Core</div>
+                    <!-- Sidenav Accordion (Dashboard)-->
+                    <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseDashboards" aria-expanded="false" aria-controls="collapseDashboards">
+                        <div class="nav-link-icon"><i data-feather="activity"></i></div>
+                        User
+                        <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapseDashboards" data-bs-parent="#accordionSidenav">
+                        <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
+                            <a class="nav-link" href="<?= $this->Url->build('/users') ?>">
+                                User list
+                                <span class="badge bg-primary-soft text-primary ms-auto">Updated</span>
+                            </a>
+                            <a class="nav-link" href="<?= $this->Url->build('/users/add') ?>">add new user</a>
+                        </nav>
+                    </div>
+                    <!-- Sidenav Heading (Custom)-->
+                    <div class="sidenav-menu-heading">business</div>
+                    <!-- Sidenav Accordion (Pages)-->
+                    <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                        <div class="nav-link-icon"><i data-feather="grid"></i></div>
+                        Apartment
+                        <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapsePages" data-bs-parent="#accordionSidenav">
+                        <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPagesMenu">
+                            <!-- Nested Sidenav Accordion (Pages -> Account)-->
+                            <a class="nav-link" href="<?= $this->Url->build('/apartments') ?>">Apartment List</a>
+                            <a class="nav-link" href="<?= $this->Url->build('/apartments/add') ?>">add new apartment</a>
+                        </nav>
+                    </div>
+                    <!-- Sidenav Accordion (Applications)-->
+                    <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseApps" aria-expanded="false" aria-controls="collapseApps">
+                        <div class="nav-link-icon"><i data-feather="globe"></i></div>
+                        Inspection
+                        <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapseApps" data-bs-parent="#accordionSidenav">
+                        <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavAppsMenu">
+                            <a class="nav-link" href="<?= $this->Url->build('/inspections') ?>">Inspection List</a>
+                            <a class="nav-link" href="<?= $this->Url->build('/inspections/add') ?>">add new Inspection</a>
+                        </nav>
+                    </div>
+
+
+            <!-- Sidenav Footer-->
+            <div class="sidenav-footer">
+                <div class="sidenav-footer-content">
+                    <div class="sidenav-footer-subtitle">Logged in as:</div>
+                    <div class="sidenav-footer-title">contractor</div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div id="layoutSidenav_content">
+        <main>
+            <header class="py-10 mb-4 bg-gradient-primary-to-secondary">
+                <div class="container-xl px-4">
+                    <div class="text-center">
+                        <h1 class="text-white">Welcome to MONO management page</h1>
+                        <p class="lead mb-0 text-white-50">Now you can start managing MONO.</p>
+                    </div>
+                </div>
+            </header>
+            <!-- Main page content-->
             <?= $this->Flash->render() ?>
             <?= $this->fetch('content') ?>
-        </div>
-    </main>
-    <footer>
-    </footer>
+        </main>
+        <footer class="footer-admin mt-auto footer-light">
+            <div class="container-xl px-4">
+                <div class="row">
+                    <div class="col-md-6 small">Copyright &copy; Your Website 2023</div>
+                    <div class="col-md-6 text-md-end small">
+                        <a href="#!">Privacy Policy</a>
+                        &middot;
+                        <a href="#!">Terms &amp; Conditions</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
 </body>
 </html>
+
