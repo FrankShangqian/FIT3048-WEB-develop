1b986ecc908d5025b9618cdcbfc7ed4581bc4cc0
AllEN <rqin0002@student.monash.edu>
2023-03-20 18:04:59 +1100

    index pages beautify

--- templates/Users/index.php
+++ templates/Users/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var iterable<\App\Model\Entity\User> $users
@@ -7,42 +8,47 @@
 <div class="users index content">
     <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Users') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('user_id') ?></th>
-                    <th><?= $this->Paginator->sort('user_fname') ?></th>
-                    <th><?= $this->Paginator->sort('user_lname') ?></th>
-                    <th><?= $this->Paginator->sort('user_prefername') ?></th>
-                    <th><?= $this->Paginator->sort('user_phone') ?></th>
-                    <th><?= $this->Paginator->sort('email') ?></th>
-                    <th><?= $this->Paginator->sort('user_type') ?></th>
-                    <th><?= $this->Paginator->sort('user_address') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($users as $user): ?>
-                <tr>
-                    <td><?= $this->Number->format($user->user_id) ?></td>
-                    <td><?= h($user->user_fname) ?></td>
-                    <td><?= h($user->user_lname) ?></td>
-                    <td><?= h($user->user_prefername) ?></td>
-                    <td><?= h($user->user_phone) ?></td>
-                    <td><?= h($user->email) ?></td>
-                    <td><?= h($user->user_type) ?></td>
-                    <td><?= h($user->user_address) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $user->user_id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->user_id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->user_id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->user_id)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+    <div class="card mb-4">
+        <div class="card-body">
+            <table class="table table-bordered" id="datatablesSimple" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th><?= $this->Paginator->sort('user_id') ?></th>
+                        <th><?= $this->Paginator->sort('user_fname') ?></th>
+                        <th><?= $this->Paginator->sort('user_lname') ?></th>
+                        <th><?= $this->Paginator->sort('user_prefername') ?></th>
+                        <th><?= $this->Paginator->sort('user_phone') ?></th>
+                        <th><?= $this->Paginator->sort('email') ?></th>
+                        <th><?= $this->Paginator->sort('user_type') ?></th>
+                        <th><?= $this->Paginator->sort('user_address') ?></th>
+                        <th class="actions"><?= __('Actions') ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($users as $user) : ?>
+                        <tr>
+                            <td><?= $this->Number->format($user->user_id) ?></td>
+                            <td><?= h($user->user_fname) ?></td>
+                            <td><?= h($user->user_lname) ?></td>
+                            <td><?= h($user->user_prefername) ?></td>
+                            <td><?= h($user->user_phone) ?></td>
+                            <td><?= h($user->email) ?></td>
+                            <td><?= h($user->user_type) ?></td>
+                            <td><?= h($user->user_address) ?></td>
+                            <td class="actions">
+                                <?= $this->Html->link(__('View'), ['action' => 'view', $user->user_id]) ?>
+                                <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->user_id]) ?>
+                                <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->user_id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->user_id)]) ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
     <div class="paginator">
         <ul class="pagination">
             <?= $this->Paginator->first('<< ' . __('first')) ?>
--- templates/Users/login.php
+++ templates/Users/login.php
@@ -1,55 +1,5 @@
 <!-- in /templates/Users/login.php -->
 <?php
-
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     0.10.0
- * @license   https://opensource.org/licenses/mit-license.php MIT License
- * @var \App\View\AppView $this
- */
-
-use Cake\Cache\Cache;
-use Cake\Core\Configure;
-use Cake\Core\Plugin;
-use Cake\Datasource\ConnectionManager;
-use Cake\Error\Debugger;
-use Cake\Http\Exception\NotFoundException;
-
-$this->disableAutoLayout();
-
-$checkConnection = function (string $name) {
-	$error = null;
-	$connected = false;
-	try {
-		$connection = ConnectionManager::get($name);
-		$connected = $connection->connect();
-	} catch (Exception $connectionError) {
-		$error = $connectionError->getMessage();
-		if (method_exists($connectionError, 'getAttributes')) {
-			$attributes = $connectionError->getAttributes();
-			if (isset($attributes['message'])) {
-				$error .= '<br />' . $attributes['message'];
-			}
-		}
-	}
-	return compact('connected', 'error');
-};
-
-
-if (!Configure::read('debug')) :
-	throw new NotFoundException(
-		'Please replace templates/Pages/home.php with your own version or re-enable debug mode.'
-	);
-endif;
-
 $this->disableAutoLayout();
 ?>
 <!DOCTYPE html>
@@ -60,11 +10,13 @@ $this->disableAutoLayout();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team member</title>
-    <link rel="stylesheet" href="log_styles.css">
     <?= $this->Html->css(['log_styles']) ?>
+    <link rel="stylesheet" type="text/css" href="styles.css?version=51">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <style>
+    </style>
 </head>
 
 <body>
@@ -80,8 +32,8 @@ $this->disableAutoLayout();
                     </fieldset>
                     <?= $this->Form->submit(__('Login', ['class' => 'btn-login'])); ?>
                     <?= $this->Form->end() ?>
-                    <?= $this->Html->link("CREATE ACCOUNT", ['action' => 'add'], ['class' => 'btn-new']) ?>
-                    <a class="btn-new" href="<?= $this->Url->build('/') ?>">Back To Home</a>
+                    <?= $this->Html->link("Create ACCOUNT", ['action' => 'add'], ['class' => 'btn-new']) ?>
+                    <a class="btn-new" href="<?= $this->Url->build('/') ?>">Home page</a>
                 </div>
             </div>
         </div>
